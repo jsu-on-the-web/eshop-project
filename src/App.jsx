7d7b79f6771d7f7f127497cd7701e08350d67421
Janet Suryadi <janet@suryadi.id.au>
2023-07-17 20:12:49 +1000

    Further scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookAtlas } from '@fortawesome/free-solid-svg-icons'
 import './App.scss'
 
 function App() {
@@ -6,24 +8,38 @@ function App() {
 
   return (
     <>
-      {/* Header */}
-      <header className=''>
-        <h1 className='font-heading'>
-          Market of Alexandria
-        </h1>
-        <h2 className='font-bold font-subheading'>An infinite library of goods, at a fraction of the price</h2>
-        {/** Navbar */}
+      <section className='grid grid-rows-4 app'>
+        {/* Header */}
+        <header className=''>
 
-      </header>
+          <h1 className='font-heading'>
+            <FontAwesomeIcon icon={faBookAtlas} className='mr-10' />
+            Market of Alexandria
+          </h1>
+          <h2 className='font-bold font-subheading'>An infinite library of goods, at a fraction of the price</h2>
+          {/** Navbar */}
 
-      {/* Body */}
-    <p className='font-body'>fssffs</p>
-      {/** Carousel */}
+        </header>
 
-      {/** Products Grid */}
+        {/* Body */}
+        <p className='font-body'>fssffs</p>
+        {/** Carousel */}
+        <section>
+          <div>
 
-      
-      {/*Footer with dummy contact info */}
+          </div>
+        </section>
+        {/** Products Grid */}
+        <section className='grid grid-cols-2 products-grid'>
+          <div className='product-card'>
+            <img className='product__image' src='' />
+            <p className='product__name'>Product Name</p>
+            <p className='product__price'>Price</p>
+          </div>
+        </section>
+
+        {/*Footer with dummy contact info */}
+</section>
     </>
   )
 }

85e79fd919da901a8382b5a6b319c336087761e3
Janet Suryadi <janet@suryadi.id.au>
2023-07-19 09:04:20 +1000

    Making the fetchAllProducts and its ID reliant counterpart

---
+++ src/services/product-services.js
@@ -0,0 +1,42 @@
+import { db } from '../config/firebase';
+import {
+    collection,
+    getDocs,
+    addDoc,
+    onSnapshot,
+    doc,
+    getDoc,
+    deleteDoc,
+    updateDoc,
+    increment,
+} from 'firebase/firestore';
+
+/*----------  Function Declaration  ----------*/
+export const fetchAllProducts = async () => {
+    const collectionReference = collection(db, 'products');
+    const querySnapshot = await getDocs(collectionReference);
+    return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+}
+
+export const fetchProductsById = async (id) => {
+    const collectionReference = collection(db, 'products', id);
+    const querySnapshot = await getDocs(collectionReference);
+
+    if (!querySnapshot.exists()) {
+        throw new Error('Document doesn\'t exist');
+    }
+
+    return { id: querySnapshot.id, ...querySnapshot.data() };
+}
+
+
+
+/*----------  Variable Declaration  ----------*/
+
+
+
+
+/*----------  Script assignment  ----------*/
+
+
+

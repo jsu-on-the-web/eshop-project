70f38bcb85eb91a7dd4814dba20014aa8d38f46b
Janet Suryadi <janet@suryadi.id.au>
2023-07-20 16:39:33 +1000

    Styling Product Cards

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./ProductCard.module.scss";
 export function ProductCard({ productImageSrc, productTitle, productPrice }) {
     return (
-        <div className='flex flex-col content-center product-card'>
+        <div className={` ${styles.productcard}`}>
             <img src="https://picsum.photos/200" className='product__image'  />
             <p className='product__name'>{productTitle}</p>
             <p className='product__price'>Price</p>

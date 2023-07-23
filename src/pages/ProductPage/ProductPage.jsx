482f4c868f6919ab3c7344e8482363611643e678
Janet Suryadi <janet@suryadi.id.au>
2023-07-24 08:43:00 +1000

    Working out the qualities dropdown

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { ProductsListContext } from '../../context/ProductsListContextProvider';
 import { useParams } from 'react-router-dom';
 import { formatDate } from '../../services/generalUtils';
@@ -6,6 +6,16 @@ import { formatDate } from '../../services/generalUtils';
 const ProductPage = () => {
     const { products } = useContext(ProductsListContext);
     const { id } = useParams();
+    const [selectedQuality, setSelectedQuality] = useState('new');
+
+    const handleQualityChange = (e) => {
+        setSelectedQuality(e.target.value);
+    }
+
+    // Reset the selected quality when a new product loads in
+    useEffect(() => {
+        setSelectedQuality('new');
+    }, [id]); // Add 'id' to the dependency array to reset when a new product is loaded
 
     // Find the item with the corresponding ID in the products array in the ProductsListContext
     const product = products.find((product) => product.id === id);
@@ -22,17 +32,34 @@ const ProductPage = () => {
         )
     }
 
+
+
     const { imageUrl, title, description, qualities, isFavourite, category, publishedDate, pageCount } = product;
+    const selectedQualityData = selectedQuality ? qualities.find((qual) => qual.id === selectedQuality) : null;
+    const selectedQualityPrice = selectedQualityData ? selectedQualityData.price : null;
+    console.log("Product Data: ", product);
     return (
         <>
             <main>
                 <section className='py-16 mx-auto max-w-8xl sm:py-24'>
                     <div className='flex'>
                         <img className='object-cover w-80' src={imageUrl} alt={title} />
-                        <div className='flex flex-col flex-1 pb-4 ml-8 border-b-2 border-timberwolf-500'>
+                        <div className='flex flex-col items-center flex-1 pb-4 ml-8 border-b-2 border-timberwolf-500'>
                             <h2 className='text-4xl italic font-heading'>{title}</h2>
 
                             {/* All the elements dependent on quality */}
+                            <select value={selectedQuality} onChange={handleQualityChange} className='max-w-sm mt-4'>
+                                <option value="">Select Quality</option>
+                                {qualities.map((qual) => (
+                                    <option key={qual.id} value={qual.id}>{qual.quality}</option>
+                                ))}
+                            </select>
+
+                            {selectedQuality && selectedQualityData && (
+                                <div className='mt-4'>
+                                    <p className='font-light font-body'>{selectedQualityPrice}</p>
+                                </div>
+                            )}
 
                             {/* End of quality dependent elements */}
                             <p></p>

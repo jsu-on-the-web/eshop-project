54204876cc5a56558d56544c595d4555148f2cf4
Janet Suryadi <janet@suryadi.id.au>
2023-07-23 23:45:36 +1000

    Adding linking for the Header and Product Cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,34 @@
 import { useState, useEffect, useContext } from 'react'
 import './App.scss'
 import { Header } from './components/Header/Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ProductPage from './pages/ProductPage/ProductPage';
-import  ProductsListContextProvider from './context/ProductsListContextProvider';
+import ProductsListContextProvider from './context/ProductsListContextProvider';
 import { fetchAllProducts } from './services/product-services';
 
 function App() {
 
     return (
         <>
-            <ProductsListContextProvider>
-                <section className='flex flex-col'>
-                    {/* Header */}
-                    <Header />
-                    {/* Body */}
-                    <BrowserRouter>
+            <BrowserRouter>
+                <ProductsListContextProvider>
+
+                    <section className='flex flex-col'>
+                        {/* Header */}
+                        <Header />
+                        {/* Body */}
+
                         <Routes>
                             <Route path='/' element={<HomePage />} />
                             <Route path='/products/:id' element={<ProductPage />} />
                         </Routes>
-                    </BrowserRouter>
 
-                    {/*Footer with dummy contact info */}
-                </section>
-            </ProductsListContextProvider>
+                        {/*Footer with dummy contact info */}
+                    </section>
+                </ProductsListContextProvider >
+
+            </BrowserRouter>
         </>
     )
 }
--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -1,6 +1,7 @@
 import {useContext} from "react";
 import { ProductCard } from "../ProductCard/ProductCard";
 import { ProductsListContext } from "../../context/ProductsListContextProvider";
+import { Link } from "react-router-dom";
 export function ProductGrid() {
    const {products} = useContext(ProductsListContext);
   return (
@@ -12,7 +13,10 @@ export function ProductGrid() {
         <ProductCard /> */}
 
         {products.map((product) => (
-          <ProductCard key={product.id} productImageSrc={product.imageUrl} productTitle={product.title} productPrice={product.qualities[3].price} productIsFavourite={product.isFavourite}/>
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <ProductCard key={product.id} productImageSrc={product.imageUrl} productTitle={product.title} productPrice={product.qualities[3].price} productIsFavourite={product.isFavourite} />
+
+            </Link>
         ))}
       </section>;
     </>

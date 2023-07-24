c144dd0e55c100f6671c7cf010dd1766dc00280b
Janet Suryadi <janet@suryadi.id.au>
2023-07-24 21:14:55 +1000

    Carousel tinkering

--- src/components/CarouselWrapper/CarouselWrapper.jsx
+++ src/components/CarouselWrapper/CarouselWrapper.jsx
@@ -1,33 +1,45 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect , useContext} from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Carousel } from '../../components/CarouselWrapper/Carousel/Carousel';
 import { ProductsListContext } from '../../context/ProductsListContextProvider';
+import CarouselButton from './CarouselButton/CarouselButton';
 
 const CarouselWrapper = () => {
     const { products } = useContext(ProductsListContext);
 
+    const featuredProducts = products ? products.filter(product => product.category === "science" || product.category === "technology") : [];
+    console.log("Featured products:", featuredProducts);
     const [startIndex, setStartIndex] = useState(0);
 
-    const visibleProducts = products.slice(startIndex, startIndex + 4);
+    const visibleProducts = featuredProducts.slice(startIndex, startIndex + 4);
     const handlePrevious = () => {
-        // The use of max here means we compare between previousIndex - 1 and 0.
-        // This is so that we don't go out of bounds and instead wrap back to 0 if we do
-        setStartIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+        setStartIndex(prevIndex => Math.max(prevIndex - 1, 0));
     };
 
     const handleNext = () => {
-        // Similar to the previous function, but we wrap around to products.length - 4
-        setStartIndex((prevIndex) => Math.min(prevIndex + 1, products.length - 4));
+        setStartIndex(prevIndex => Math.min(prevIndex + 1, featuredProducts.length - 4));
     };
+
+    useEffect(() => {
+        console.log("All products:", products);
+    }, [products]);
+
+    useEffect(() => {
+        console.log("Visible products:", visibleProducts);
+    }, [visibleProducts]);
+
     return (
-        <section className='m-5 mb-17'>
+        <section className='flex m-5 mb-17'>
+            <CarouselButton className='w-12 h-12' direction='left' onClick={handlePrevious} />
             <Carousel
                 products={visibleProducts}
                 currentIndex={startIndex}
                 onPrevious={handlePrevious}
-                onNext={handleNext} />
+                onNext={handleNext}
+            />
+            <CarouselButton direction='right' onClick={handleNext} />
         </section>
-    )
+    );
 }
 
-export default CarouselWrapper
+export default CarouselWrapper;
